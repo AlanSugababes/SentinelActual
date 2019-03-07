8778aac5a105bdbd74c498224b78e928ea4b43f2
AlanSugababes <AlanSugababes@gmail.com>
2019-03-07 17:26:18 +0000

    changing naming convention to match standards

--- SentinelActual-Dev/server/scripts/ExpressWordCount/pdftojson.js
+++ SentinelActual-Dev/server/scripts/ExpressWordCount/pdftojson.js
@@ -1,6 +1,14 @@
 const pdf = require("pdf-parse");
 const fs = require("fs");
 const dir = "./pdfs/";
+const choki = require("chokidar");
+let watcher = chokidar.watch(dir,{ignored:/^\./, persistent: true});
+
+watcher.on("change", function(path){
+  console.log("Dir", path, "has been changed");
+  PDFToJSONConverter();
+}
+
 
 function PDFToJSONConverter() {
   let file = fs.readdirSync(dir);
